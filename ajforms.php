9d4837b5d8c15098e18921eed834bb1801db1fb2
sandman <ssn.jnk@gmail.com>
2026-05-07 10:15:39 -0400

    Release AJ Forms 0.1.20

--- ajforms.php
+++ ajforms.php
@@ -3,7 +3,7 @@
  * Plugin Name:       AJ Forms
  * Plugin URI:        https://github.com/ssnanda/ajforms
  * Description:       A custom WordPress form builder plugin for building forms, collecting entries, and managing workflows inside WordPress.
- * Version: 0.1.19
+ * Version: 0.1.20
  * Author:            itSpector
  * Author URI:        https://itspector.com
  * Update URI:        false
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( ! defined( 'AJFORMS_VERSION' ) ) {
-	define( 'AJFORMS_VERSION', '0.1.19' );
+	define( 'AJFORMS_VERSION', '0.1.20' );
 }
 
 if ( ! defined( 'AJFORMS_PLUGIN_DIR' ) ) {
