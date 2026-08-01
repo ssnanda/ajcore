0a644f6fe73d054205438d63761c5e0d3b08435a
sandman <ssn.jnk@gmail.com>
2026-08-01 16:26:19 -0400

    Release AJ Core 0.7.185

--- ajcore.php
+++ ajcore.php
@@ -3,7 +3,7 @@
  * Plugin Name:       AJ Core
  * Plugin URI:        https://github.com/ssnanda/ajcore
  * Description:       A modular WordPress business toolkit for forms, payments, portals, auth, CRM, and automations.
- * Version: 0.7.184
+ * Version: 0.7.185
  * Author:            IT Spector LLC
  * Author URI:        https://itspector.com
  * Update URI:        false
@@ -18,7 +18,7 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( ! defined( 'AJCORE_VERSION' ) ) {
-	define( 'AJCORE_VERSION', '0.7.184' );
+	define( 'AJCORE_VERSION', '0.7.185' );
 }
 
 if ( ! defined( 'AJCORE_PLUGIN_DIR' ) ) {
@@ -225,7 +225,7 @@ if ( ! function_exists( 'ajforms_get_settings_defaults' ) ) {
 			'upos_thermo_client_secret'      => '',
 			'upos_thermo_redirect_uri'       => '',
 			'upos_thermo_location_id'        => '',
-			'upos_thermo_device_ids'         => '',
+			'upos_thermo_device_ids'         => 'LCC-48A2E6C087EE,LCC-48A2E6C087F0,LCC-5CFCE1418EA4,LCC-5CFCE10F604B,LCC-5CFCE12AC73C,LCC-5CFCE12AC700,LCC-5CFCE12AC788,LCC-5CFCE12AC902,LCC-5CFCE10C335C,LCC-5CFCE10F6061,LCC-5CFCE145B2E1',
 			'upos_thermo_refresh_token'      => '',
 			'stripe_mode'                   => 'test',
 			'stripe_sandbox_publishable_key' => '',
